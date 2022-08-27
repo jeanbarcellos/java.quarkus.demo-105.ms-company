920d2fbf4b5e08933aeb593fd1b10f40712f7abc
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-08-27 19:27:38 -0300

    Adição do Consul Client
    - Adição da lib no pom.xml
    - Adição do Producer e LoadBalancer para a filtragem no Client
    - Nova configuração em application.properties
    - Ajustes em DepartmentServvice

---
+++ department/src/main/java/com/jeanbarcellos/ms/department/client/LoadBalancedFilter.java
@@ -0,0 +1,52 @@
+package com.jeanbarcellos.ms.department.client;
+
+import java.net.URI;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.core.UriBuilder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.HealthClient;
+import com.orbitz.consul.model.health.ServiceHealth;
+
+public class LoadBalancedFilter implements ClientRequestFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoadBalancedFilter.class);
+
+    private Consul consulClient;
+
+    private AtomicInteger counter = new AtomicInteger();
+
+    public LoadBalancedFilter(Consul consulClient) {
+        this.consulClient = consulClient;
+    }
+
+    @Override
+    public void filter(ClientRequestContext requestContext) {
+        URI uri = requestContext.getUri();
+
+        HealthClient healthClient = consulClient.healthClient();
+
+        List<ServiceHealth> instances = healthClient.getHealthyServiceInstances(uri.getHost()).getResponse();
+
+        instances.forEach(it -> LOGGER.info("Instance: uri={}:{}",
+                it.getService().getAddress(),
+                it.getService().getPort()));
+
+        ServiceHealth instance = instances.get(counter.getAndIncrement() % instances.size());
+
+        URI u = UriBuilder.fromUri(uri)
+                .host(instance.getService().getAddress())
+                .port(instance.getService().getPort())
+                .build();
+
+        requestContext.setUri(u);
+    }
+
+}

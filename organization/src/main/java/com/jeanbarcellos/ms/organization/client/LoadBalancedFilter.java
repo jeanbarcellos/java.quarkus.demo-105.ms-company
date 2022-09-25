dbe5bcb44f0032508e16a349bdceedaddfe06691
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-09-24 21:26:34 -0300

    organization - AJustes nos clients
    - Obtendo isntâncias do Consult

---
+++ organization/src/main/java/com/jeanbarcellos/ms/organization/client/LoadBalancedFilter.java
@@ -0,0 +1,56 @@
+package com.jeanbarcellos.ms.organization.client;
+
+import java.net.URI;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.core.UriBuilder;
+
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.HealthClient;
+import com.orbitz.consul.model.health.ServiceHealth;
+
+public class LoadBalancedFilter implements ClientRequestFilter {
+
+    private Consul consulClient;
+
+    private AtomicInteger counter = new AtomicInteger();
+
+    public LoadBalancedFilter(Consul consulClient) {
+        this.consulClient = consulClient;
+    }
+
+    @Override
+    public void filter(ClientRequestContext requestContext) {
+        URI uri = requestContext.getUri();
+
+        HealthClient healthClient = consulClient.healthClient();
+
+        List<ServiceHealth> instances = healthClient.getHealthyServiceInstances(uri.getHost()).getResponse();
+
+        ServiceHealth instance = getInstance(instances);
+
+        URI newUri = createUri(uri, instance);
+
+        requestContext.setUri(newUri);
+    }
+
+    private ServiceHealth getInstance(List<ServiceHealth> instances) {
+        Integer random = selectInstance(instances.size());
+        return instances.get(random);
+    }
+
+    private Integer selectInstance(Integer qtd) {
+        return counter.getAndAccumulate(qtd, (cur, n) -> cur >= n - 1 ? 0 : cur + 1);
+    }
+
+    private URI createUri(URI uri, ServiceHealth instance) {
+        return UriBuilder.fromUri(uri)
+                .host(instance.getService().getAddress())
+                .port(instance.getService().getPort())
+                .build();
+    }
+
+}
--- organization/src/main/java/com/jeanbarcellos/ms/organization/producers/ConsulProducer.java
+++ organization/src/main/java/com/jeanbarcellos/ms/organization/producers/ConsulProducer.java
@@ -1,8 +1,17 @@
 package com.jeanbarcellos.ms.organization.producers;
 
+import java.net.URISyntaxException;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
+import org.apache.http.client.utils.URIBuilder;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+
+import com.jeanbarcellos.ms.organization.client.DepartmentClient;
+import com.jeanbarcellos.ms.organization.client.EmployeeClient;
+import com.jeanbarcellos.ms.organization.client.LoadBalancedFilter;
 import com.orbitz.consul.Consul;
 
 @ApplicationScoped
@@ -10,5 +19,37 @@ public class ConsulProducer {
 
     @Produces
     Consul consulClient = Consul.builder().build();
+
+    @ConfigProperty(name = "client.employee-api.uri")
+    String employeeUri;
+
+    @ConfigProperty(name = "client.department-api.uri")
+    String departmentUri;
+
+    @Produces
+    LoadBalancedFilter employeeFilter = new LoadBalancedFilter(consulClient);
+
+    @Produces
+    LoadBalancedFilter departmentFilter = new LoadBalancedFilter(consulClient);
+
+    @Produces
+    EmployeeClient employeeClient() throws URISyntaxException {
+        URIBuilder builder = new URIBuilder(employeeUri);
+
+        return RestClientBuilder.newBuilder()
+                .baseUri(builder.build())
+                .register(employeeFilter)
+                .build(EmployeeClient.class);
+    }
+
+    @Produces
+    DepartmentClient departmentClient() throws URISyntaxException {
+        URIBuilder builder = new URIBuilder(departmentUri);
+
+        return RestClientBuilder.newBuilder()
+                .baseUri(builder.build())
+                .register(departmentFilter)
+                .build(DepartmentClient.class);
+    }
 
 }

49e1363e88868da2fd8241a03e5f5a3a95a6ae96
Jean Silva de Barcellos <jean.barcellos@cresol.coop.br>
2024-08-12 06:37:26 -0300

    employee - melhorias
    - Ajustes no consul
    - application.properties
    - Dockerfile

--- employee/src/main/java/com/jeanbarcellos/project105/employee/producers/ConsulProducer.java
+++ employee/src/main/java/com/jeanbarcellos/project105/employee/producers/ConsulProducer.java
@@ -3,12 +3,33 @@ package com.jeanbarcellos.project105.employee.producers;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import com.google.common.net.HostAndPort;
 import com.orbitz.consul.Consul;
 
+/**
+ * Service Provider/Producer para
+ *
+ * Registrar o Client Consult
+ *
+ * @author Jean Silva de Barcellos
+ */
 @ApplicationScoped
 public class ConsulProducer {
 
+    @ConfigProperty(name = "consul.host")
+    protected String host;
+
+    @ConfigProperty(name = "consul.port")
+    protected int port;
+
     @Produces
-    Consul consulClient = Consul.builder().build();
+    Consul consulClient() {
+        return Consul.builder()
+                .withHostAndPort(HostAndPort.fromParts(host, port))
+                .build();
+
+    }
 
 }

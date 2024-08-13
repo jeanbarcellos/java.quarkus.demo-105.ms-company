ba49d19ecca15ac4f019d9199513dec81abb0dfd
Jean Barcellos <jeanbarcellos@hotmail.com>
2024-08-13 08:42:34 -0300

    department - melhorias

--- department/src/main/java/com/jeanbarcellos/ms/entities/Department.java
+++ department/src/main/java/com/jeanbarcellos/ms/entities/Department.java
@@ -17,6 +17,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 
 @Getter
@@ -24,6 +25,7 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @Builder
 @Entity
 @Table(name = "\"department\"")

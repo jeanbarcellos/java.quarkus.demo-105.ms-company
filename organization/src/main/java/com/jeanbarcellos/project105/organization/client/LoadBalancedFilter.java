26b358f49811074ba986dff9fd60853f86965721
Jean Barcellos <jeanbarcellos@hotmail.com>
2024-08-13 08:42:38 -0300

    employee - melhorias
    - Ajustes no consul
    - application.properties
    - Dockerfile

--- organization/src/main/java/com/jeanbarcellos/project105/organization/client/LoadBalancedFilter.java
+++ organization/src/main/java/com/jeanbarcellos/project105/organization/client/LoadBalancedFilter.java
@@ -8,11 +8,16 @@ import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.core.UriBuilder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.orbitz.consul.Consul;
 import com.orbitz.consul.HealthClient;
 import com.orbitz.consul.model.health.ServiceHealth;
 
 public class LoadBalancedFilter implements ClientRequestFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoadBalancedFilter.class);
 
     private Consul consulClient;
 
@@ -26,9 +31,17 @@ public class LoadBalancedFilter implements ClientRequestFilter {
     public void filter(ClientRequestContext requestContext) {
         URI uri = requestContext.getUri();
 
+        LOGGER.info("Get instances of '{}'.", uri.getHost());
+
         HealthClient healthClient = consulClient.healthClient();
 
         List<ServiceHealth> instances = healthClient.getHealthyServiceInstances(uri.getHost()).getResponse();
+
+        this.logInstances(instances);
+
+        if (instances.isEmpty()) {
+            LOGGER.error("The '{}' instance not found.", uri.getHost());
+        }
 
         ServiceHealth instance = getInstance(instances);
 
@@ -37,9 +50,29 @@ public class LoadBalancedFilter implements ClientRequestFilter {
         requestContext.setUri(newUri);
     }
 
+    private void logInstances(List<ServiceHealth> instances) {
+        LOGGER.info("Instances:");
+
+        var index = 0;
+        for (ServiceHealth instance : instances) {
+            LOGGER.info("  index={} uri={}:{}",
+                    index,
+                    instance.getService().getAddress(),
+                    instance.getService().getPort());
+            index++;
+        }
+    }
+
     private ServiceHealth getInstance(List<ServiceHealth> instances) {
         Integer random = selectInstance(instances.size());
-        return instances.get(random);
+
+        var instance = instances.get(random);
+
+        LOGGER.info("Selected:");
+        LOGGER.info("  index={} uri={}:{}",
+                random, instance.getService().getAddress(), instance.getService().getPort());
+
+        return instance;
     }
 
     private Integer selectInstance(Integer qtd) {
